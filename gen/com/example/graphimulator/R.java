bf364238094892142921abd80aa9810430737026
shafiurcse02 <shafiurcse@gmail.com>
2014-11-22 12:06:54 +0600

    shafiur last

--- gen/com/example/graphimulator/R.java
+++ gen/com/example/graphimulator/R.java
@@ -23,15 +23,23 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo=0x7f020001;
     }
     public static final class id {
-        public static final int about_button=0x7f080001;
-        public static final int action_settings=0x7f080003;
-        public static final int help_button=0x7f080002;
-        public static final int start_button=0x7f080000;
+        public static final int about_button=0x7f080005;
+        public static final int about_text=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int back=0x7f080001;
+        public static final int button1=0x7f080003;
+        public static final int help=0x7f080006;
+        public static final int help_button=0x7f080004;
+        public static final int start_button=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int about=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int f_page=0x7f030002;
+        public static final int help=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -40,6 +48,7 @@ public final class R {
         public static final int about_text=0x7f050005;
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int cancel=0x7f050006;
         public static final int hello_world=0x7f050001;
         public static final int help_text=0x7f050004;
         public static final int start_text=0x7f050003;
